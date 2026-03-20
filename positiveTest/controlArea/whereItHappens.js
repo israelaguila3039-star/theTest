4cb2b069ebbe019ed02f206bc202a8be17d33d9c
israelaguila3039-star <israel.aguila3039@stu.mtec.edu>
2026-03-20 08:16:14 -0600

    not as bad

--- positiveTest/controlArea/whereItHappens.js
+++ positiveTest/controlArea/whereItHappens.js
@@ -4,11 +4,11 @@ import weAreHere from '../stuff/weGotHere.js'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open()
+        await thePlaceILogIn.open()
 
-        await LoginPage.login('standard_user', 'secret_sauce')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
+        await thePlaceILogIn.login('standard_user', 'secret_sauce')
+        await expect(weAreHere.flashAlert).toBeExisting()
+        await expect(weAreHere.flashAlert).toHaveText(
             expect.stringContaining('Swag Labs'))
     })
 })
